92f7f73f75e775e6702aa40182e19f0e4ea44971
Yuki Kitagishi <mokona965@gmail.com>
2016-12-26 22:26:07 +0900

    161226

--- teikou.py
+++ teikou.py
@@ -35,10 +35,13 @@ def run(event) :
 			value4 = teikou(float(value1), float(value2), float(value3))
 			EditBox4.insert(Tkinter.END,value4)
 
+def finish(event) :
+	sys.exit("System exit")
+
 root = Tkinter.Tk()
 
 root.title("Software Title")
-root.geometry("400x300")
+root.geometry("400x150")
 
 Static1_1 = Tkinter.Label(text="電源電圧")
 Static1_1.place(x=x1, y=y1)
@@ -85,12 +88,19 @@ value4 = EditBox4.get()
 Static4_2 = Tkinter.Label(text=u"\u03a9")
 Static4_2.place(x=x3, y=y4)
 
-Button = Tkinter.Button(text="Button")
-Button.bind("<Button-1>",run) 
+Button1 = Tkinter.Button(text="Calculation")
+Button1.bind("<Button-1>",run) 
 # <Button-1>: click
 # <Button-2>: wheel-click
 # <Button-3>: right-click
-Button.place(x=x4, y=y5)
+Button1.place(x=x4, y=y5)
+
+Button2 = Tkinter.Button(text="Exit")
+Button2.bind("<Button-1>",finish) 
+# <Button-1>: click
+# <Button-2>: wheel-click
+# <Button-3>: right-click
+Button2.place(x=x4, y=y5+30)
 
 
 root.mainloop()
@@ -109,64 +119,3 @@ def heiretsu(r_list) :
 def chokuretsu(r_list) :
 	s = sum(r_list)
 	return s
-"""
-if __name__ == "__main__" :
-
-	root = Tkinter.Tk()
-	root.title("elect")
-	root.geometry("400x300")
-
-	Val1 = Tkinter.BooleanVar()
-	Val2 = Tkinter.BooleanVar()
-	Val3 = Tkinter.BooleanVar()
-
-	Val1.set(False)
-	Val2.set(False)
-	Val3.set(False)
-
-	CheckBox1 = Tkinter.Checkbutton(text=u"項目1", variable=Val1)
-	CheckBox1.pack()
-
-	CheckBox2 = Tkinter.Checkbutton(text=u"項目2", variable=Val2)
-	CheckBox2.pack()
-
-	CheckBox3 = Tkinter.Checkbutton(text=u"項目3", variable=Val3)
-	CheckBox3.pack()
-
-
-	root.mainloop()
-	root2.mainloop()
-	exit()
-
-	flag = int(input("計算の種類を選んでください\n抵抗値の計算: 1，並列抵抗の計算: 2，直列抵抗の計算: 3\n>>>"))
-	r_list = []
-
-	if flag == 1 :
-		v1 = input("電源電圧(V)を入力してください\n>>>")
-		v2 = input("流したい電圧(V)を入力してください\n>>>")
-		ma = input("流したい電流(mA)を入力してください\n>>>")
-		r = teikou(float(v1), float(v2), float(ma))
-		print("抵抗は%sです" %r)
-		exit()
-
-	elif flag == 2 :
-		while True :
-			r = input("抵抗値を入力してください\n終了する場合は'-1'を入力してください\n>>>")
-			if int(r) == -1 :
-				exit()
-			else :
-				r_list.append(float(r))
-		r = heiretsu(r_list)
-		print("並列抵抗の合計値は%sです" %r)
-
-	elif flag == 3 :
-		while True :
-			r = input("抵抗値を入力してください\n終了する場合は'-1'を入力してください\n>>>")
-			if int(r) == -1 :
-				if len(r_list) != 0 :
-					r = chokuretsu(r_list)
-					print("直列抵抗の合計値は%sです" %r)
-				exit()
-			else :
-				r_list.append(float(r))
-"""
